3611dba0a2c71b859f1cbe7f877924c87271aa03
fakhfakh7 <yosrafakhfakh2016@gmail.com>
2020-12-23 23:03:42 +0100

    Yes

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import './App.css';
 import Navabar from'./component/Navabar';
+import Home from './component/Home';
 
 
 function App() {
   return (
     <div className="App">
-      <Navabar/>
+      <Navabar/> 
+      <Home/>
     </div>
   );
 }
